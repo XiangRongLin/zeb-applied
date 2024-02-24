7a09a8ccc62b56e79cb25742443d151884bfcbd5
Xiang Rong Lin <41164160+XiangRongLin@users.noreply.github.com>
2024-02-24 11:02:05 +0100

    Create project with spring initializer

---
+++ src/main/kotlin/com/example/zebapplied/ZebAppliedApplication.kt
@@ -0,0 +1,11 @@
+package com.example.zebapplied
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ZebAppliedApplication
+
+fun main(args: Array<String>) {
+  runApplication<ZebAppliedApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/zebapplied/ZebAppliedApplicationTests.kt
@@ -0,0 +1,12 @@
+package com.example.zebapplied
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ZebAppliedApplicationTests {
+
+  @Test
+  fun contextLoads() {
+  }
+}
